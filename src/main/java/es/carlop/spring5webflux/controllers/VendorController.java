bb51e084cb50095764c74e78add42602e8fa6e66
Carlos López <yo@carlop.es>
2021-06-23 17:25:20 +0200

    Implement POST and PUT for Vendors

--- src/main/java/es/carlop/spring5webflux/controllers/VendorController.java
+++ src/main/java/es/carlop/spring5webflux/controllers/VendorController.java
@@ -3,6 +3,7 @@ package es.carlop.spring5webflux.controllers;
 import es.carlop.spring5webflux.config.BaseURLs;
 import es.carlop.spring5webflux.domain.Vendor;
 import es.carlop.spring5webflux.repositories.VendorRepository;
+import org.reactivestreams.Publisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -29,4 +30,17 @@ public class VendorController {
     Mono<Vendor> getVendorById(@PathVariable String id) {
         return vendorRepository.findById(id);
     }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Void> createVendor(@RequestBody Publisher<Vendor> vendorStream) {
+        return vendorRepository.saveAll(vendorStream).then();
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Vendor> updateVendor(@PathVariable String id, @RequestBody Vendor vendor) {
+        vendor.setId(id);
+        return vendorRepository.save(vendor);
+    }
+
 }
--- src/test/java/es/carlop/spring5webflux/controllers/VendorControllerTest.java
+++ src/test/java/es/carlop/spring5webflux/controllers/VendorControllerTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 public class VendorControllerTest {
 
@@ -50,4 +52,34 @@ public class VendorControllerTest {
                 .expectBody(Vendor.class);
     }
 
+
+    @Test
+    void createVendor() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().firstName("Name").lastName("Last name").build()));
+
+        Mono<Vendor> vendorToSaveMono = Mono.just(Vendor.builder().firstName("Name").build());
+
+        webTestClient.post()
+                .uri(BaseURLs.VENDORS_URL)
+                .body(vendorToSaveMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    void updateVendor() {
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(Vendor.builder().firstName("Name").lastName("Last name").build()));
+
+        Mono<Vendor> vendorToUpdateMono = Mono.just(Vendor.builder().firstName("Desc").lastName("Last name").build());
+
+        webTestClient.put()
+                .uri(BaseURLs.VENDORS_URL + "/someid")
+                .body(vendorToUpdateMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
 }
